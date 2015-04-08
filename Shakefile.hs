fae1d84330804f8946a7e631f7ec0345d408c1e9
Masashi Fujita <objectxtreme@gmail.com>
2015-04-08 19:07:20 +0900

    Add build scripts

---
+++ Shakefile.hs
@@ -0,0 +1,32 @@
+import           Development.Shake          (cmd, need, phony, putNormal,
+                                             removeFilesAfter, shakeArgs,
+                                             shakeFiles, shakeOptions, want,
+                                             (%>))
+import           Development.Shake.Command  ()
+import           Development.Shake.FilePath (dropDirectory1, exe, (-<.>), (<.>),
+                                             (</>))
+import           Development.Shake.Util     (needMakefileDependencies)
+
+main :: IO ()
+main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+    want ["_build/reactive-sample" <.> exe, "_build/pipe-sample" <.> exe]
+
+    phony "clean" $ do
+        putNormal "Cleaning files in _build"
+        removeFilesAfter "_build" ["//*"]
+
+    "_build/reactive-sample" <.> exe %> \out -> do
+        let obj = out -<.> "o"
+        need [obj]
+        cmd "cc -o" [out] obj
+
+    "_build/pipe-sample" <.> exe %> \out -> do
+        let obj = out -<.> "o"
+        need [obj]
+        cmd "cc -o" [out] obj
+
+    "_build//*.o" %> \out -> do
+        let c = "src" </> (dropDirectory1 $ out -<.> "c")
+        let m = out -<.> "m"
+        () <- cmd "cc -c" [c] "-o" [out] "-MMD -MF" [m]
+        needMakefileDependencies m
